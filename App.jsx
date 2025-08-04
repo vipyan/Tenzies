e89140f82faa9bd139894f2959ea026ae278dec1
vipyan <vipinkaniyanthara@gmail.com>
2025-08-04 14:52:28 -0400

    added new features
    removed old ul for dice.
    Added a new button to restart game and added a counter to show effort

--- App.jsx
+++ App.jsx
@@ -1,74 +1,95 @@
-import { useState, useRef, useEffect } from "react"
-import Die from "./Die"
-import { nanoid } from "nanoid"
-import Confetti from "react-confetti"
+import { useState, useRef, useEffect } from "react";
+import Die from "./Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
-    const [dice, setDice] = useState(() => generateAllNewDice())
-    const buttonRef = useRef(null)
+  const [dice, setDice] = useState(() => generateAllNewDice());
+  const buttonRef = useRef(null);
 
-    const gameWon = dice.every(die => die.isHeld) &&
-        dice.every(die => die.value === dice[0].value)
-        
-    useEffect(() => {
-        if (gameWon) {
-            buttonRef.current.focus()
-        }
-    }, [gameWon])
+    const [effort, setEffort] = useState(0);
 
-    function generateAllNewDice() {
-        return new Array(10)
-            .fill(0)
-            .map(() => ({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-                id: nanoid()
-            }))
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
+
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus();
+    }
+  }, [gameWon]);
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
+  }
+
+    function rollDice() {
+    setEffort((prev) => prev + 1);
+    if (!gameWon) {
+      setDice((oldDice) =>
+        oldDice.map((die) =>
+          die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    } else {
+        setDice(generateAllNewDice())
+        setEffort(0)
     }
     
-    function rollDice() {
-        if (!gameWon) {
-            setDice(oldDice => oldDice.map(die =>
-                die.isHeld ?
-                    die :
-                    { ...die, value: Math.ceil(Math.random() * 6) }
-            ))
-        } else {
-            setDice(generateAllNewDice())
-        }
+    
+  }
+  
+  
+    
+  function hold(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  const diceElements = dice.map((dieObj) => (
+    <Die
+      key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
+    />
+  ));
+    
+    function exit() {
+        setDice(generateAllNewDice())
+        setEffort(0)
     }
 
-    function hold(id) {
-        setDice(oldDice => oldDice.map(die =>
-            die.id === id ?
-                { ...die, isHeld: !die.isHeld } :
-                die
-        ))
-    }
-
-    const diceElements = dice.map(dieObj => (
-        <Die
-            key={dieObj.id}
-            value={dieObj.value}
-            isHeld={dieObj.isHeld}
-            hold={() => hold(dieObj.id)}
-        />
-    ))
-
-    return (
-        <main>
-            {gameWon && <Confetti />}
-            <div aria-live="polite" className="sr-only">
-                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
-            </div>
-            <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all animals match! Click an animal to keep it from changing on the next roll.</p>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
-                {gameWon ? "New Game" : "Roll"}
-            </button>
-        </main>
-    )
+  return (
+    <main>
+      {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && (
+          <p>Congratulations! You won! Press "New Game" to start again.</p>
+        )}
+      </div>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all animals match! Click an animal to keep it from changing
+        on the next roll.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll"}
+          </button>
+          <p className="roll-count">Roll count: {effort}</p>
+          {effort > 0 && !gameWon && <button
+                                
+                className="exit-button button"
+                onClick={exit}>Exit and start new game</button>}
+          
+    </main>
+  );
 }
--- Die.jsx
+++ Die.jsx
@@ -1,45 +1,17 @@
 export default function Die(props) {
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
-    }
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
 
-    
-
-function getDiceEmoji(value) {
-  let face;
-  switch (value) {
-    case 1:
-      face = "🐭";
-      break;
-    case 2:
-      face = "🐶";
-      break;
-    case 3:
-      face = "🐰";
-      break;
-    case 4:
-      face = "🦊";
-      break;
-    case 5:
-      face = "🐼";
-      break;
-    case 6:
-      face = "🦁";
-      break;
-    default:
-      face = "❓";  
-  }
-  return face;
-}
-
-    
-    return (
-        <button 
-            style={styles}
-            onClick={props.hold}
-            aria-pressed={props.isHeld}
-            aria-label={`Die with value ${props.value}, 
+  return (
+    <button
+      style={styles}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
             ${props.isHeld ? "held" : "not held"}`}
-        >{getDiceEmoji(props.value)}</button>
-    )
+    >
+      {props.value}
+    </button>
+  );
 }
